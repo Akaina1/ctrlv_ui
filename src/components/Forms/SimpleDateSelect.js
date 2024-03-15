d0d06d749d980f7a080ae9231e55522e8ddf4921
Aaron <erinadost@gmail.com>
2024-03-15 17:55:30 -0400

    formatting

--- src/components/Forms/SimpleDateSelect.js
+++ src/components/Forms/SimpleDateSelect.js
@@ -66,7 +66,13 @@ const SimpleDateSelect = ({ title, description, onSubmit, descriptionTextColor, 
         )}
         {onSubmit && (
             <button
-            className="relative font-julius-sans-one text-lg block navbar:flex items-center px-4 py-2 mb-4 navbar:mb-0 before:content-[''] before:absolute before:inset-0 before:border-b-4 before:border-r-4 before:border-l-2 before:border-t-2 before:border-black before:transition before:duration-200 hover:before:border-b-2 hover:before:border-r-2 hover:scale-95 hover:bg-white hover:bg-opacity-20"
+            className="relative font-julius-sans-one text-lg block 
+            navbar:flex items-center px-4 py-2 mb-4 navbar:mb-0 
+            before:content-[''] before:absolute before:inset-0 
+            before:border-b-4 before:border-r-4 before:border-l-2 
+            before:border-t-2 before:border-black before:transition 
+            before:duration-200 hover:before:border-b-2 hover:before:border-r-2 
+            hover:scale-95 hover:bg-white hover:bg-opacity-20 cursor-pointer"
             style={{ color: buttonTextColor, backgroundColor: buttonColor }}
             aria-label="Submit"
             onClick={handleSubmit}
